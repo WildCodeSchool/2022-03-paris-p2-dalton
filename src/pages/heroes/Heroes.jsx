e1a5218c3cd48c2d4937399ad01ce29f6506a940
Stefannee <stephanie.caussier@gmail.com>
2022-04-20 10:44:22 +0200

    init heroes page

---
+++ src/pages/heroes/Heroes.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import HeroList from "../../data/HeroList";
+
+export const Heroes = ({ hero }) => {
+  return (
+    <div>
+      <HeroList></HeroList>
+    </div>
+  );
+};
+
+export default Heroes;
