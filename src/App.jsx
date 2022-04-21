fdb7cfc4d914d997457679a1ab59f779782a7408
Eric <pradier_eric@orange.fr>
2022-04-21 09:52:06 +0200

    add Link and Route

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Header from "./components/header/Header";
 import Home from "./pages/home/Home.jsx";
+import Heroes from "./pages/heroes/Heroes.jsx";
 import Fight from "./pages/fight/Fight.jsx";
-import CharacterDetails from "./data/CharacterDetails";
+/*import Match from "./pages/match/Match.jsx";*/
 import Shop from "./pages/shop/Shop.jsx";
+import CharacterDetails from "./data/CharacterDetails";
 
 function App() {
   return (
@@ -12,13 +14,12 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        {/* <Route path="heroes" element={<Heroes />  */}
-        {/* <Route path="Match" element={<Match /> */}
+        <Route path="heroes" element={<Heroes />} />
         <Route path="fight" element={<Fight />} />
-        {/* <Route path="Shop" element={<Shop /> */}
+    {/*  <Route path="match" element={<Match />} /> */}
+        <Route path="Shop" element={<Shop /> } />
         {/*            <Route path="/character/:id/fight" element={<CharacterDetails />} />*/}
-        <Route path="/:id/fight" />
-        <Route path="shop" element={<Shop />} />
+       {/* <Route path="/:id/fight" /> */}
       </Routes>
     </div>
   );
