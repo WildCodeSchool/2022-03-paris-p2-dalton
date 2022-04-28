bf6b82f519387f4e12d6c533ec98021ef7248b09
Stefannee <stephanie.caussier@gmail.com>
2022-04-28 15:53:02 +0200

    init footer

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <div className="footer">
+      <p>🦸 Made with love by The Dalton’s </p>
+    </div>
+  );
+}
