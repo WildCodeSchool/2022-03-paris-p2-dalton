7d38886e8ed55fe361f37588dcdc66b4ba40b2b5
Stefannee <stephanie.caussier@gmail.com>
2022-04-28 09:44:16 +0200

    search barre done

--- src/pages/heroes/Heroes.jsx
+++ src/pages/heroes/Heroes.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
-import HeroList from "../../data/HeroList";
 import "./Heroes.css";
 
 export default function () {
@@ -10,6 +9,9 @@ export default function () {
   // ici c'est comme si on fetch/aller chercher nos superheroes
   const [searchResults, setSearchResults] = useState([]);
 
+  function imageRandom(plop) {
+    return;
+  }
   // ici tu vas créer une fonction qui déclenche et stocke un randomNumber ->
   // un nombre au hasard : maths floor maths random
   // au chargement du composant
@@ -33,44 +35,40 @@ export default function () {
 
   return (
     <div className="container-body">
+      <div className="img-banner"></div>
       <div className="container-input">
-        <div>
-          <img className="img-banner" />
-          {/* //ici randomNumber à mettre */}
-        </div>
-        <h2>Your heroes by name</h2>
+        <h2>Heroes list</h2>
         <p>You can search and filter your favorite heroes</p>
         <input
           type="text"
           value={query}
-          placeholder="SEARCH"
+          placeholder="SEARCH YOUR HERO"
           onChange={(e) => setQuery(e.target.value)}
         />
       </div>
-      {searchResults
-        .filter((hero) => hero.name.toLowerCase().startsWith(`${query}`))
-        .map((hero) => (
-          <div key={hero.id}>
-            <div className="container-img">
-              {" "}
-              <Link to={{ pathname: `/hero/${hero.id}` }}>
-                <img
-                  className="img-heroes"
-                  src={hero.picture}
-                  alt={hero.name}
-                />
-              </Link>
-            </div>
+      <div className="container-heroes">
+        {searchResults
+          .filter((hero) => hero.name.toLowerCase().startsWith(`${query}`))
+          .map((hero) => (
+            <div key={hero.id}>
+              <div className="container-img">
+                <Link to={{ pathname: `/hero/${hero.id}` }}>
+                  <img
+                    className="img-heroes"
+                    src={hero.picture}
+                    alt={hero.name}
+                  />
+                </Link>
+              </div>
 
-            <div>
               <div className="border_blue"></div>
               <div className="container_hero">
                 <h3>{hero.name}</h3>
                 <h4>{hero.race}</h4>
               </div>
             </div>
-          </div>
-        ))}
+          ))}
+      </div>
     </div>
   );
 }
