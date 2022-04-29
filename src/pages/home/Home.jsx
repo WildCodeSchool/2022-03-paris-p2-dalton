3b312e87a70b38e24e46b4cf38d41447a9f19b82
Stefannee <stephanie.caussier@gmail.com>
2022-04-29 12:28:39 +0200

    init branch

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,13 @@
-import './Home.css';
+import "./Home.css";
+import { Link } from "react-router-dom";
 
 function Home() {
   return (
-    <div className='guard'>
+    <div className="container-image">
+      <h1>love.war.shop</h1>
+      <button className="button-home">
+        <p className="p-button">discover thoses heroes</p>
+      </button>
     </div>
   );
 }
