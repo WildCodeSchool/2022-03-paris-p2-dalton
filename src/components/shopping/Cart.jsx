90fa2839c671fb5e76084b69a890a60003d7de04
Vaishnavi Bala <vaishnavi_bala@hotmail.fr>
2022-05-09 13:48:44 +0200

    suppression commentaires

--- src/components/shopping/Cart.jsx
+++ src/components/shopping/Cart.jsx
@@ -55,21 +55,3 @@ const Cart = ({cart, updateCart}) => {
   }
 
 export default Cart
-
-
-
-
-//     <div className='whs-cart'>
-//       <button onClick={() => setIsOpen(false)}>Close the basket</button>
-//         <h2>Panier</h2>
-//         <div className='cart'>
-//           Card 1 : {cardPrice}€
-//           <button onClick={() => updateCart(cart + 1)}>Add</button>
-//         </div>
-//         <h3>Total : {cardPrice * cart}€</h3>
-//         <button onClick={() => updateCart(0)}>Clear the basket</button>
-//     </div>
-//   ) : (
-//     <button className='btn-open-cart' onClick={() => setIsOpen(true)}>Open the basket<img src={basket} alt='basket' width='30' heigth='30' /></button>
-//   )
-// }
